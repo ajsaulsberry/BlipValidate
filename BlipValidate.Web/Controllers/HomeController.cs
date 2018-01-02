cf670734bc3c54c1a0dc8768db4f282ed40ecb0d
ajsaulsberry <ajsaulsberry@gmail.com>
2018-01-02 14:37:51 -0500

    Fixed namespace.

--- BlipValidate.Web/Controllers/HomeController.cs
+++ BlipValidate.Web/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using BlipValidate.Web.Models;
+using BlipValidate.Web.ViewModels;
 
 namespace BlipValidate.Web.Controllers
 {
